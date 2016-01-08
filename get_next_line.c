90146d251280f37d88b7881145be4d246202a255
John <Admin@macbook-pro-de-jonathan.home>
2016-01-08 13:17:41 +0100

    debug

--- get_next_line.c
+++ get_next_line.c
@@ -12,33 +12,99 @@
 
 #include "get_next_line.h"
 
-int	get_read_line(char *buff_tmp)
+size_t	get_len_stop(const char *read_buf, int start, char c_stop)
+{
+	size_t	cpt;
+
+	if (!read_buf)
+		return (0);
+	cpt = 0;
+	while (read_buf[start] && read_buf[start] != c_stop)
+	{
+		start++;
+		cpt++;
+	}
+	return (cpt);
+}
+
+int	get_return_line(char *read_buf, char **line, t_struct buf_res)
+{
+	int		len;
+	char	*tmp;
+	char	*line_tmp;
+
+	len = 0;
+	if (ft_strchr(read_buf, '\n') == 0)
+	{
+		line_tmp = *line;
+		*line = ft_strjoin(line_tmp, read_buf);
+		free(line_tmp);
+	}
+	else
+	{
+		len = get_len_stop(read_buf, 0, '\n');
+		tmp = ft_strsub(read_buf, 0, len);
+		line_tmp = *line;
+		*line = ft_strjoin(line_tmp, tmp);
+		free(line_tmp);
+		free(tmp);
+		tmp = ft_strsub(read_buf, len + 1, (ft_strlen(read_buf) - len));
+		free(buf_res->read_buf);
+		buf_res->read_buf = tmp;
+		return (1);
+	}
+	return (0);
+}
+
+int	get_read_line(int fd, char *buf_tmp, char **line, t_struct *buf_res)
 {
 	int	i;
 
 	i = 0;
-	while (buff_tmp[i] && buff_tmp[i] != '\n')
+	if (!(buf_res->read_buf))
+		buf_res->read_buf = ft_strdup("");
+	if (get_return_line(buf_res->read_buf, line, buf_res) == 1)
+		return (buf_res->read = 1);
+	free(buf_res->read_buf);
+	buf_res->read_buf = ft_strdup("");
+	while (i = read(fd, buf_tmp, BUFF_SIZE))
 	{
-		if (buff_tmp[i] == '\n')
-			return(0);
-		i++;
+		buf_tmp[i] = 0;
+		if (get_return_line(buf_tmp, line, buf_res) == 1)
+			return (1);
+		buf_res->read = 1;
 	}
-	return (1);
+	if (i == -1)
+		return (-1);
+	if (buf_res->read == 1)
+	{
+		buf_res->read = 0;
+		if (ft_strcmp("", *line) != 0)
+			return (1);
+	}
+	return (0);
 }
 
 int	get_next_line(int const fd, char **line)
 {
-	static char		*buff = NULL;
-	char			buff_tmp[BUFF_SIZE + 1];
-	int				i;
-	int				ret;
+	static t_struct	*buf_res;
+	char			*buf_tmp;
+	int				res;
 
-	while (ret > 0 && fd)
+	if (!line)
+		return (-1);
+	if ((buf_res = (t_struct*)malloc(sizeof(t_struct))) == NULL)
+		return (-1);
+	if (!(buf_tmp = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1))))
+		return (-1);
+	buf_tmp[0] = 0;
+	*line = ft_strdup("");
+	res = get_read_line(fd, buf_tmp, line, buf_res);
+	if ((res == 0) || (res == -1))
 	{
-		ret = read(fd, buff_tmp, BUFF_SIZE);
-		buff_tmp[BUFF_SIZE] = '\0';
-		if (!(get_read_line(buff_tmp))
-			return (0);
+		free(buf_res->read_buf);
+		free(buf_res);
 	}
-	return (0);
+	free(buf_tmp);
+	return (res);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -14,8 +14,21 @@
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 32;
 
-# include "libft.h"
+# include "Libft/libft.h"
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+typedef struct		s_struct
+{
+	int				read;
+	char			*read_buf;
+}					t_struct;
 
 int	get_next_line(int const fd, char **line);
+int	get_read_line(int fd, char *buf_tmp, char **line, t_struct *buf_res);
+int	get_return_line(char *read_buf, char **line, t_struct buf_res);
+size_t	get_len_stop(const char *read_buf, int start, char c_stop);
 
 #endif
